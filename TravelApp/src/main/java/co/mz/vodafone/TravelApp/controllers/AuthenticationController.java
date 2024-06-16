88a138962fcbbfd486580610f72e7b454b729a28
Focas Kandulo <focas.kandulo@ambisig.com>
2024-06-16 20:37:05 +0200

    doc: api tags auth controller

--- TravelApp/src/main/java/co/mz/vodafone/TravelApp/controllers/AuthenticationController.java
+++ TravelApp/src/main/java/co/mz/vodafone/TravelApp/controllers/AuthenticationController.java
@@ -3,6 +3,7 @@ import co.mz.vodafone.TravelApp.dtos.LoginResponse;
 import co.mz.vodafone.TravelApp.dtos.UserAccountDto;
 import co.mz.vodafone.TravelApp.interfaces.IAuthenticationService;
 import co.mz.vodafone.TravelApp.entity.UserAccount;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,11 +18,13 @@ public class AuthenticationController {
     public AuthenticationController(IAuthenticationService authenticationService) {
         _authenticationService = authenticationService;
     }
+    @Tag(name = "post", description = "User registration")
     @PostMapping("/signup")
     public ResponseEntity<UserAccount> register(@RequestBody UserAccountDto dto) {
         UserAccount userAccount = _authenticationService.signup(dto);
         return ResponseEntity.ok(userAccount);
     }
+    @Tag(name = "post", description = "User login")
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> authenticate(@RequestBody UserAccountDto dto) {
         var loginResponse =_authenticationService.login(dto);
