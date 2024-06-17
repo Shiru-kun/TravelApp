34cab77a668c284f885f13c0d2132d81c49d8df1
Focas Kandulo <focas.kandulo@ambisig.com>
2024-06-17 09:48:40 +0200

    wip: adjust styles structure

--- travelapp-frontend/src/components/WeatherCard.tsx
+++ travelapp-frontend/src/components/WeatherCard.tsx
@@ -1,12 +1,51 @@
 import React from 'react';
 import styles from '../styles/pages/home/home.module.scss';
+import { WeatherData } from '../types';
 
-const WeatherCard = () => {
-  return (
-    <div className={styles.tabContent}>
- 
-    </div>
-  );
+const WeatherCard =({ data }: { data: WeatherData }) => {
+    const { weather, main, wind, clouds, sys, name } = data;
+    const weatherIconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}.png`;
+
+    return (
+        <div className={styles.cardsContainer}>
+          <div className={styles.card}>
+            <div className={styles.cardHeader}>
+              <h2>{name}, {sys.country}</h2>
+              <img src={weatherIconUrl} alt={weather[0].description} />
+            </div>
+            <div className={styles.cardContent}>
+              <p><strong>Temperature:</strong> {(main.temp - 273.15).toFixed(1)}°C</p>
+              <p><strong>Feels Like:</strong> {(main.feels_like - 273.15).toFixed(1)}°C</p>
+              <p><strong>Min Temperature:</strong> {(main.temp_min - 273.15).toFixed(1)}°C</p>
+              <p><strong>Max Temperature:</strong> {(main.temp_max - 273.15).toFixed(1)}°C</p>
+            </div>
+          </div>
+    
+          <div className={styles.card}>
+            <div className={styles.cardContent}>
+              <p><strong>Pressure:</strong> {main.pressure} hPa</p>
+              <p><strong>Humidity:</strong> {main.humidity}%</p>
+              <p><strong>Visibility:</strong> {data.visibility / 1000} km</p>
+            </div>
+          </div>
+    
+          <div className={styles.card}>
+            <div className={styles.cardContent}>
+              <p><strong>Wind Speed:</strong> {wind.speed} m/s</p>
+              <p><strong>Wind Direction:</strong> {wind.deg}°</p>
+              <p><strong>Cloudiness:</strong> {clouds.all}%</p>
+            </div>
+          </div>
+    
+          <div className={styles.card}>
+            <div className={styles.cardContent}>
+              <p><strong>Weather:</strong> {weather[0].description}</p>
+              <p><strong>Sunrise:</strong> {new Date(sys.sunrise * 1000).toLocaleTimeString()}</p>
+              <p><strong>Sunset:</strong> {new Date(sys.sunset * 1000).toLocaleTimeString()}</p>
+            </div>
+          </div>
+        </div>
+      );
 };
 
 export default WeatherCard;
--- travelapp-frontend/src/components/WeatherTab.tsx
+++ travelapp-frontend/src/components/WeatherTab.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
 import styles from '../styles/pages/home/home.module.scss';
+import WeatherCard from './WeatherCard';
 
 const WeatherTab = () => {
+  const weatherData = {
+    coord: {
+      lon: 32.5675,
+      lat: -25.9662
+    },
+    weather: [
+      {
+        id: 801,
+        main: "Clouds",
+        description: "few clouds",
+        icon: "02n"
+      }
+    ],
+    main: {
+      temp: 290.31,
+      feels_like: 290.38,
+      temp_min: 290.31,
+      temp_max: 290.31,
+      pressure: 1024,
+      humidity: 88,
+      sea_level: 0,
+      grnd_level: 0
+    },
+    visibility: 10000,
+    wind: {
+      speed: 0.0,
+      deg: 0,
+      gust: 0.0
+    },
+    clouds: {
+      all: 20
+    },
+    sys: {
+      country: "MZ",
+      sunrise: 1718512499,
+      sunset: 1718550363
+    },
+    name: "Maputo"
+  };
   return (
     <div className={styles.tabContent}>
- 
+       <WeatherCard data={weatherData} />
     </div>
   );
 };
