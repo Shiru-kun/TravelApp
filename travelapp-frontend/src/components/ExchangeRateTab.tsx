c9cbfd7f8fa04062bd0fba77e999de5b7e248278
Focas Kandulo <focas.kandulo@ambisig.com>
2024-06-20 16:10:00 +0200

    styles fix deslocated card on mobile view

--- travelapp-frontend/src/components/ExchangeRateTab.tsx
+++ travelapp-frontend/src/components/ExchangeRateTab.tsx
@@ -3,24 +3,28 @@ import { useExchangeRateQuery } from '../services/exchange-rate-service';
 import { getCurrencyByCountryCode } from '../services/worldbank-service';
 import { useTranslation } from 'react-i18next';
 
-type Props ={countryCode:string };
-const ExchangeRateTab = ({countryCode }:Props ) => {
+type Props = { countryCode: string };
+const ExchangeRateTab = ({ countryCode }: Props) => {
   const _countryCode = getCurrencyByCountryCode(countryCode);
   const { t } = useTranslation();
 
-  const sendRequest:boolean = _countryCode?true:false;
-  const {data} = useExchangeRateQuery(_countryCode??countryCode,sendRequest);
+  const sendRequest: boolean = _countryCode ? true : false;
+  const { data } = useExchangeRateQuery(_countryCode ?? countryCode, sendRequest);
   return (
     <div className={styles.tabContent}>
-    <div className={styles.card}>
-      <h2>{t('ExchangeRate')}</h2>
-      {data?.success  ? (
-        <span> 1 {data?.base} =  {_countryCode} {data?.rates?.[`${_countryCode}`]?.toFixed(2)}</span>
-      ) : (
-        <div className={`${styles.blurText} ${styles.tabContent}`} style={{margin:10}}> </div>
-      )}
+      <div className={styles.cardsContainer}>
+
+        <div className={styles.card}>
+          <h2>{t('ExchangeRate')}</h2>
+          {data?.success ? (
+            <span> 1 {data?.base} =  {_countryCode} {data?.rates?.[`${_countryCode}`]?.toFixed(2)}</span>
+          ) : (
+            <div className={`${styles.blurText} ${styles.tabContent}`} style={{ margin: 10 }}> </div>
+          )}
+        </div>
+      </div>
     </div>
-    </div>
+
   );
 };
 
